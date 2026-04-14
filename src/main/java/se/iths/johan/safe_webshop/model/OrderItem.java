ffd1997d05d32f81fc7c7d9c336fe59e7e21fd94
EllinorIths <ellinor.hoijer@iths.se>
2026-04-14 10:09:15 +0200

    Uppdaterat CartController och lagt till lombok ist för getter/setter

--- src/main/java/se/iths/johan/safe_webshop/model/OrderItem.java
+++ src/main/java/se/iths/johan/safe_webshop/model/OrderItem.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class OrderItem {
     @Id
@@ -21,35 +25,4 @@ public class OrderItem {
     @ManyToOne
     private Order order;
 
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public void setOrder(Order order) {
-        this.order = order;
-    }
 }
--- src/main/java/se/iths/johan/safe_webshop/model/Product.java
+++ src/main/java/se/iths/johan/safe_webshop/model/Product.java
@@ -3,7 +3,11 @@ package se.iths.johan.safe_webshop.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 public class Product {
 
@@ -16,44 +20,4 @@ public class Product {
     private String category;
     private String imageUrl;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
 }
