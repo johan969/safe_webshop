0617f927b95485c32c8bcb65c4b36979f35ad894
johan <johan.persson@iths.se>
2026-04-16 11:04:37 +0200

    lägger till hasConsent variabel i homeController så den inte är null

--- src/main/java/se/iths/johan/safe_webshop/controller/HomeController.java
+++ src/main/java/se/iths/johan/safe_webshop/controller/HomeController.java
@@ -2,6 +2,7 @@ package se.iths.johan.safe_webshop.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @GetMapping
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("hasConsent", false);
         return "index";
     }
 
